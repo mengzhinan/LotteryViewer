fb06db413956f779ed4584ff477621fc062dad54
“杜科” <duke@zhihu.com>
2021-03-25 14:42:21 +0800

    init

--- app/src/main/java/com/lotteryviewer/android/LotteryApplication.kt
+++ app/src/main/java/com/lotteryviewer/android/LotteryApplication.kt
@@ -2,7 +2,9 @@ package com.lotteryviewer.android
 
 import android.content.Context
 import androidx.multidex.MultiDex
+import com.lotteryviewer.base.app.AppBuildConfig
 import com.lotteryviewer.base.app.BaseApplication
+import com.lotteryviewer.home.ui.activities.MainActivity
 
 /**
  * @Author: duke
@@ -14,6 +16,17 @@ class LotteryApplication : BaseApplication() {
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
         MultiDex.install(this)
+
+        AppBuildConfig.newBuilder()
+            .DEBUG(BuildConfig.DEBUG)
+            .APPLICATION_ID(BuildConfig.APPLICATION_ID)
+            .MAIN_ACTIVITY_NAME(MainActivity::class.java.name)
+            .BUILD_TYPE(BuildConfig.BUILD_TYPE)
+            .FLAVOR(BuildConfig.FLAVOR)
+            .VERSION_CODE(BuildConfig.VERSION_CODE)
+            .VERSION_NAME(BuildConfig.VERSION_NAME)
+
+
     }
 
 }
